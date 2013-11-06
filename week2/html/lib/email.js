fd1b7130fcc35770df6220c4cfc4acb8b1fa7bb8
Tiffany Duhl <tiffanyduhl@hotmail.com>
2013-11-06 13:45:19 -0700

    trying to avoid GitHub filing week5's html assignment in the 'week2' branch that i deleted

--- week2/html/lib/email.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	
-    $('li').find('.nopad').on('click', function() {
-    var email = $('<p><strong>tiffanyduhl@hotmail.com</strong></p>');
-    $(this).closest('li').append(email);
-    $(this).remove();
-
-});
-});
